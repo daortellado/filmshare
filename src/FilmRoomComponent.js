9b228241cd76955047e8bb715d4169945c3a1823
daortellado <daortellado@gmail.com>
2024-04-26 14:10:23 -0700

    filter logic changes, mostly
    now apply filter button alters result set, and filter combinations are allowed

--- src/FilmRoomComponent.js
+++ src/FilmRoomComponent.js
@@ -4,27 +4,18 @@ import axios from "axios";
 import Cookies from "universal-cookie";
 import ReactPlayer from "react-player";
 import "./App.css";
-import AWS from "aws-sdk";
-import emailjs from 'emailjs-com';
-
-const MediaConvert = require("aws-sdk/clients/mediaconvert");
-const { ImageInserter } = require("aws-sdk/clients/mediaconvert");
 
 const cookies = new Cookies();
 const token = cookies.get("TOKEN");
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-});
 
 export default function FilmRoomComponent() {
   const [videolist, setVideoList] = useState([]);
   const [result, setResult] = useState([]);
   const [selectedClips, setSelectedClips] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [downloadUrl, setDownloadUrl] = useState(null);
   const [userEmail, setUserEmail] = useState("");
+  const [selectedFilters, setSelectedFilters] = useState({ game: "", tags: [] });
+  const [tempFilters, setTempFilters] = useState({ game: "", tags: [] }); // Temporary filter state
 
   useEffect(() => {
     const configuration = {
@@ -96,119 +87,155 @@ export default function FilmRoomComponent() {
   const uniquePlayerTags = [...playerTags].sort();
   const uniqueOtherTags = [...otherTags].sort();
 
+  const applyFilters = () => {
+    setResult(
+      videolist.filter((video) => {
+        const gameFilter = selectedFilters.game ? video.game === selectedFilters.game : true;
+        const tagFilter = selectedFilters.tags.length === 0 ? true : selectedFilters.tags.every((tag) =>
+          video.tags.some((videoTag) => videoTag.trim().toLowerCase() === tag)
+        );
+        return gameFilter && tagFilter;
+      })
+    );
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [selectedFilters]);
+
+  const handleApplyFilters = () => {
+    setSelectedFilters({ ...tempFilters }); // Apply the temporary filters
+  };
+
   async function handleDownload() {
     setIsLoading(true);
-  
+
     // Extract the links of the selected clips
     const playlistContent = selectedClips.map((clip) => clip.link);
-  
-    try {  
+
+    try {
       // Data to send to the backend
       const requestData = {
         playlistContent, // Selected clips
         userEmail, // User's email
       };
-  
+
       // Send the data to the backend
       const response = await axios.post('https://filmshare-fd851c149ec7.herokuapp.com/create-mysquadreel', requestData, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-  
+
       // Handle the response if needed
-  
+
     } catch (error) {
       console.error("Error creating job:", error);
     } finally {
       setIsLoading(false);
     }
-  }  
+  }
   
+  const handleFilterChange = (filterType, value) => {
+    setTempFilters((prevFilters) => {
+      if (filterType === "game") {
+        return prevFilters.game === value
+          ? { ...prevFilters, game: "" } // Toggle off the game filter if already selected
+          : { ...prevFilters, game: value };
+      } else {
+        const newTags = prevFilters.tags.includes(value)
+          ? prevFilters.tags.filter((tag) => tag !== value)
+          : [...prevFilters.tags, value];
+        return { ...prevFilters, tags: newTags };
+      }
+    });
+  };
+
   return (
     <div className="text-center">
+      <div className="text-center mb-4">
+        <Button variant="primary" onClick={handleApplyFilters}>
+          Apply Filters
+        </Button>
+      </div>
       <h4>Select a <b>game</b> by clicking one of the buttons below:</h4>
       <div className="App">
         <p>
           {uniquegames.map((game) => (
             <button
-              class="btn btn-outline-light"
+              className={`btn btn-outline-light ${tempFilters.game === game ? 'active' : ''}`}
               value={game}
-              onClick={(e) => {
-                setResult(videolist.filter((video) => video.game === game));
-                console.log(result);
-              }}
+              onClick={() => handleFilterChange("game", game)}
             >
               {game}
             </button>
           ))}
         </p>
-        <h4>Or select a <b>tag</b> by clicking one of the buttons below:</h4>
+        <h4>Or select <b>tags</b> by clicking one of the buttons below:</h4>
         <p>
           <p><h5><i>Players</i></h5></p>
           {uniquePlayerTags.map((tag) => (
-            <button key={tag} class="btn btn-outline-light" value={tag} onClick={() => {
-              setResult(videolist.filter(video => video.tags.some(videoTag => videoTag.trim().toLowerCase() === tag)));
-              setSelectedClips([]); // Deselect all on tag click
-              console.log(result);
-            }}>
+            <button
+              key={tag}
+              className={`btn btn-outline-light ${tempFilters.tags.includes(tag) ? 'active' : ''}`}
+              value={tag}
+              onClick={() => handleFilterChange("tag", tag)}
+            >
               {tag}
             </button>
           ))}
-  
+
           <p><h5><i>Other Tags</i></h5></p>
           {uniqueOtherTags.map((tag) => (
-            <button key={tag} class="btn btn-outline-light" value={tag} onClick={() => {
-              setResult(videolist.filter(video => video.tags.some(videoTag => videoTag.trim().toLowerCase() === tag)));
-              setSelectedClips([]); // Deselect all on tag click
-              console.log(result);
-            }}>
+            <button
+              key={tag}
+              className={`btn btn-outline-light ${tempFilters.tags.includes(tag) ? 'active' : ''}`}
+              value={tag}
+              onClick={() => handleFilterChange("tag", tag)}
+            >
               {tag}
             </button>
           ))}
         </p>
-        {result.length > 0 && (  
-        <> 
-    <Button variant="outline-primary" size="sm" onClick={() => setSelectedClips(result)}>  
-      Select All
-    </Button>
-    <Button variant="outline-danger" size="sm" onClick={() => setSelectedClips([])}>
-      Clear All Selections
-    </Button>
-          <h4>Selected Videos:</h4>
-          <ul class="no-bullets">
-            {selectedClips.map((video) => (
-              <li key={video.link}>{video.videoname} ({video.game})</li>
-            ))}
-          </ul>
-          <Button variant="primary" disabled={!selectedClips.length}>
-            {downloadUrl ? (
-              <p>MySquadReel disabled - link already sent to email. Reload page to create another.</p>
-            ) : (
-              <div>
-                <input
-                  type="email"
-                  placeholder="Enter your email address"
-                  value={userEmail}
-                  onChange={(e) => setUserEmail(e.target.value)}
-                />
-                <Button
-                  variant="primary"
-                  onClick={handleDownload}
-                  className="create-squadreel-btn"
-                  disabled={!selectedClips.length || isLoading} // Disable button when isLoading is true
-                  >
-                  {isLoading ? (
-                    "MySquadReel is processing - check your email in a few minutes for your download link!"
-                  ) : (
-                    "Create MySquadReel"
-                  )}
-                </Button>
-              </div>
-            )}
-          </Button>
-        </>
-      )}
+        {result.length === 0 && (
+          <p>Click "Apply Filters" to see the video list.</p>
+        )}
+        {result.length > 0 && (
+          <>
+            <Button variant="outline-primary" size="sm" onClick={() => setSelectedClips(result)}>
+              Select All
+            </Button>
+            <Button variant="outline-danger" size="sm" onClick={() => setSelectedClips([])}>
+              Clear All Selections
+            </Button>
+            <h4>Selected Videos:</h4>
+            <ul className="no-bullets">
+              {selectedClips.map((video) => (
+                <li key={video.link}>{video.videoname} ({video.game})</li>
+              ))}
+            </ul>
+            <div>
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+              />
+              <Button
+                variant="primary"
+                onClick={handleDownload}
+                className="create-squadreel-btn"
+                disabled={!selectedClips.length || isLoading} // Disable button when isLoading is true
+              >
+                {isLoading ? (
+                  "MySquadReel is processing - check your email in a few minutes for your download link!"
+                ) : (
+                  "Create MySquadReel"
+                )}
+              </Button>
+            </div>
+          </>
+        )}
         <p>
           {result.map((video) => (
             <div className="paddeddiv">
